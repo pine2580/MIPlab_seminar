a7f241aa471f0440dbd7746b0f00c27ffe6c383e
SongRan <pine0814@gmail.com>
2016-03-21 01:47:51 +0900

    반지름 계산 수정후 좌표 구하기, rad각도

--- circletoline/circletoline/소스.cpp
+++ circletoline/circletoline/소스.cpp
@@ -56,20 +56,20 @@ void Free(colorimg** image, int width, int height)
 		free(image[i]);
 	free(image);
 }
-void circleXY(int i, int r1_x, int r1_y, int o_x, int o_y, int x, int y, int *a_x, int *a_y, int*b_x, int*b_y, int*c_x, int*c_y, int*d_x, int*d_y, float *sub_x, float *sub_y)
+void circleXY(int i, float r, int o_x, int o_y, int x, int y, int *a_x, int *a_y, int*b_x, int*b_y, int*c_x, int*c_y, int*d_x, int*d_y, float *sub_x, float *sub_y)
 {
-	double rad;
-	float r;
-	//float rx[280], ry[280];// 이동하는 원의 좌표
-	float* rx = (float*)calloc(y, sizeof(float));
+	double angle;
+	float* rx = (float*)calloc(y, sizeof(float)); 	// 이동하는 원의 좌표
 	float* ry = (float*)calloc(y, sizeof(float));
+	angle = (360.0 / x)*i;
+	float rad;
+	rad = angle*3.14 / 180;
+	
+
 	for (int j = 0; j < y; j++) //y는 lineimg높이
 	{
-		//rad = ((360.0/x)*i*3.14) / 180;
-		rad = (360.0 / x)*i;
-		r = sqrt(pow(r1_x + j - o_x, 2) + pow(r1_y + j - o_y, 2));
-		rx[j] = o_x + ((r)*cos(rad));//원의 좌표
-		ry[j] = o_y + ((r)*sin(rad));
+		rx[j] = o_x + ((r+j)*cos(rad));//원의 좌표
+		ry[j] = o_y + ((r+j)*sin(rad));
 		a_x[j] = int(rx[j]);
 		a_y[j] = int(ry[j]);
 		b_x[j] = int(rx[j]) + 1;
@@ -80,7 +80,6 @@ void circleXY(int i, int r1_x, int r1_y, int o_x, int o_y, int x, int y, int *a_
 		d_y[j] = int(ry[j]) + 1;
 		sub_x[j] = rx[j] - int(rx[j]);
 		sub_y[j] = ry[j] - int(ry[j]);
-
 	}
 }
 void bilinearInterpolation(colorimg** CImg, colorimg** img, int x, int y, int *a_x, int *a_y, int*b_x, int*b_y, int*c_x, int*c_y, int*d_x, int*d_y, float *sub_x, float *sub_y)
@@ -100,17 +99,15 @@ void main()
 
 	int cir_width, line_width;
 	int cir_height, line_height;
-	colorimg** CImg = ReadImage_color("C:\\Users\\김송란\\Documents\\네이트온 받은 파일\\숙제2-테스트영상\\test.bmp", &cir_width, &cir_height);
-
-	line_width = 10000;
-	line_height = 280;
-	colorimg** lineimg = (colorimg**)IntAlloc2(line_width, line_height);
+	colorimg** CImg = ReadImage_color("C:\\Users\\김송란\\Documents\\네이트온 받은 파일\\숙제2-테스트영상\\크기변환cats.jpg", &cir_width, &cir_height);
 
 	int o_x = 1332, o_y = 1334; //원의 중심
 	int r1_x = 2350, r1_y = 1334, r2_x = 2610, r2_y = 1334; //원 반지름 1, 2
-															//int o_x = 6660, o_y = 6664; //원의 중심
-															//int r1_x = 11740, r1_y = 6664;
-
+	//int o_x = 6660, o_y = 6664; //원의 중심
+	//int r1_x = 11740, r1_y = 6664, r2_x=13052, r2_y=6664;
+	
+	line_width = 10000;
+	line_height = r2_x-r1_x;
 	int* a_x = (int*)calloc(line_height, sizeof(int));
 	int* a_y = (int*)calloc(line_height, sizeof(int));
 	int* b_x = (int*)calloc(line_height, sizeof(int));
@@ -122,13 +119,15 @@ void main()
 	float* sub_x = (float*)calloc(line_height, sizeof(float));
 	float* sub_y = (float*)calloc(line_height, sizeof(float));
 	float r; //반지름
+	r = r1_x - o_x;
+	colorimg** lineimg = (colorimg**)IntAlloc2(line_width, line_height);
 
 	for (int i = 0; i <line_width; i++)//가로로 한줄씩
 	{
-		circleXY(i, r1_x, r1_y, o_x, o_y, line_width, line_height, a_x, a_y, b_x, b_y, c_x, c_y, d_x, d_y, sub_x, sub_y);
+		circleXY(i, r, o_x, o_y, line_width, line_height, a_x, a_y, b_x, b_y, c_x, c_y, d_x, d_y, sub_x, sub_y);
 		bilinearInterpolation(CImg, lineimg, i, line_height, a_x, a_y, b_x, b_y, c_x, c_y, d_x, d_y, sub_x, sub_y);
 	}
-	WriteImage_color("C:\\Users\\김송란\\Documents\\네이트온 받은 파일\\숙제2-테스트영상\\test_out2.bmp", lineimg, line_width, line_height);
+	WriteImage_color("C:\\Users\\김송란\\Documents\\네이트온 받은 파일\\숙제2-테스트영상\\고양이.bmp", lineimg, line_width, line_height);
 
 	Free(CImg, cir_width, cir_height);
 	Free(lineimg, line_width, line_height);
