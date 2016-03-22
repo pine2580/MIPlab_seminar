8b0f983416b70d53389cf0e4796e7651c107922e
SongRan <pine0814@gmail.com>
2016-03-22 18:38:29 +0900

    각도 예외 처리 해줌, 결과 나옴 문제점 한 줄 값이 들어가지 않음

--- circletoline/circletoline/소스.cpp
+++ circletoline/circletoline/소스.cpp
@@ -103,19 +103,24 @@ void lineXY(int r2,double lr,int y, int x,int line_height,int line_width,int o_x
 		ly = lr-r2;
 		//lx = acos((x - o_x+1) / lr);
 		//lx = pow(cos((x - o_x + 1) / lr), -1);
-		cos_tmp = pow(cos(x / lr), -1);
-			lx = cos_tmp / (360.0 / line_width);
-			*la_x = int(lx);
-			*la_y = int(ly);
-			*lb_x = int(lx) + 1;
-			*lb_y = int(ly);
-			*lc_x = int(lx);
-			*lc_y = int(ly) + 1;
-			*ld_x = int(lx) + 1;
-			*ld_y = int(ly) + 1;
-			*sub_lx = lx - int(lx);
-			*sub_ly = ly - int(ly);
-		
+		cos_tmp = acos(((double)x - o_x) / lr);
+		//sin_tmp = asin(((double)y - o_y) / lr);
+		double angle = cos_tmp*180.0 / 3.14;
+		if(y>o_x)
+			lx = (360.0-angle) / (360.0 / line_width);
+		else
+			lx = angle / (360.0 / line_width);
+		//printf("%lf \n", cos_tmp);
+		*la_x = int(lx);
+		*la_y = int(ly);
+		*lb_x = int(lx) + 1;
+		*lb_y = int(ly);
+		*lc_x = int(lx);
+		*lc_y = int(ly) + 1;
+		*ld_x = int(lx) + 1;
+		*ld_y = int(ly) + 1;
+		*sub_lx = lx - int(lx);
+		*sub_ly = ly - int(ly);
 		
 }
 void bilinearInterpolation2(colorimg** CImg2, colorimg** img, int y, int x, int la_x, int la_y, int lb_x, int lb_y, int lc_x, int lc_y, int ld_x, int ld_y, float sub_lx, float sub_ly)
@@ -133,11 +138,11 @@ void main()
 
 	int cir_width, line_width;
 	int cir_height, line_height;
-	//int o_x = 1332, o_y = 1334; //원의 중심
-	//int r1_x = 2350, r1_y = 1334, r2_x = 2610, r2_y = 1334; //원 반지름 1, 2
+	int o_x = 1332, o_y = 1334; //원의 중심
+	int r1_x = 2350, r1_y = 1334, r2_x = 2610, r2_y = 1334; //원 반지름 1, 2
 
-	int o_x = 6660, o_y = 6664; //원의 중심
-	int r1_x = 11740, r1_y = 6664, r2_x=13052, r2_y=6664;
+	//int o_x = 6660, o_y = 6664; //원의 중심
+	//int r1_x = 11740, r1_y = 6664, r2_x=13052, r2_y=6664;
 	
 	/*
 	colorimg** CImg = ReadImage_color("C:\\Users\\김송란\\Documents\\네이트온 받은 파일\\숙제2-테스트영상\\circle2.bmp", &cir_width, &cir_height);
@@ -170,10 +175,10 @@ void main()
 	
 
 
-	colorimg** CImg2 = ReadImage_color("C:\\Users\\김송란\\Documents\\네이트온 받은 파일\\숙제2-테스트영상\\확인2.bmp", &line_width, &line_height);
+	colorimg** CImg2 = ReadImage_color("C:\\Users\\김송란\\Documents\\네이트온 받은 파일\\숙제2-테스트영상\\확인.bmp", &line_width, &line_height);
 	int r2 = r1_x - o_x;
-	int circle_width = o_x + line_height+r2+10;
-	int circle_height = o_y + line_height +r2+10;
+	int circle_width = o_x + line_height+r2+20;
+	int circle_height = o_y + line_height +r2+20;
 	double lr;
 	colorimg** circleimg = (colorimg**)IntAlloc2(circle_width, circle_height);
 	int la_x;
@@ -187,13 +192,12 @@ void main()
 	float sub_lx;
 	float sub_ly;
 
-	for (int i = 11000; i < circle_height; i++)
-		for (int j =1600 ; j < circle_width; j++)
+	for (int i = 0; i < circle_height; i++)
+		for (int j =0 ; j < circle_width; j++)
 		{
 			lr = sqrt(pow(j - o_x, 2) + pow(i - o_y, 2));//원의 방정식을 만족하는 x,y를 찾는다.
 			if (r2<lr&&lr<r2+line_height-3)
 			{
-				//printf("%lf", acos(1993 / lr));
 				lineXY(r2,lr,i, j, line_height, line_width, o_x, o_y, &la_x, &la_y, &lb_x, &lb_y, &lc_x, &lc_y, &ld_x, &ld_y, &sub_lx, &sub_ly);
 				bilinearInterpolation2(CImg2, circleimg, i, j, la_x, la_y, lb_x, lb_y, lc_x, lc_y, ld_x, ld_y, sub_lx, sub_ly);
 			}
@@ -204,8 +208,9 @@ void main()
 				circleimg[j][i].r = 255;
 			}
 		}
+	
 
-	WriteImage_color("C:\\Users\\김송란\\Documents\\네이트온 받은 파일\\숙제2-테스트영상\\linetocircle3.bmp", circleimg, circle_width, circle_height);
+	WriteImage_color("C:\\Users\\김송란\\Documents\\네이트온 받은 파일\\숙제2-테스트영상\\linetocircle6.bmp", circleimg, circle_width, circle_height);
 	Free(circleimg, circle_width, circle_height);
 	Free(CImg2, line_width, line_height);
 }
